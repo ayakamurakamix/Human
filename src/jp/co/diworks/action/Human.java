9412d6bdda64a631135535193bd11677838a590b
ayaka <ayaka.murakami1204@gmail.com>
2023-11-17 20:56:23 +0900

    Java20-3

--- src/jp/co/diworks/action/Human.java
+++ src/jp/co/diworks/action/Human.java
@@ -4,5 +4,8 @@ public class Human {
 	public static void main(String[]args) {
 		HumanName abc=new HumanName();
 		System.out.println(abc.getName());
+		
+		HumanAge efg=new HumanAge();
+		System.out.println(efg.getAge());
 	}
 }
